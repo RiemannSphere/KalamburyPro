ecfa5237b9afdc4bd2f153ad61be011b97e01d88
piotrKolodziejski <pmkolodziejski@gmail.com>
2020-08-06 19:02:35 +0200

    chuj

---
+++ WebContent/app/js/util-service.js
@@ -0,0 +1,32 @@
+const Util = {
+    APP_NAME: 'KalamburyPro-1',
+    API: {
+        REST: 'http',
+        WS: 'ws'
+    },
+    RES: {
+        DRAW: 'draw',
+        CHAT: 'chat',
+        LOGIN: 'rest/login'
+    },
+    IP: {
+        LOCAL: 'localhost'
+    },
+    ROUTE: {
+        GAME: 'app/game-component/game.html',
+        LOGIN: 'index.html'
+    },
+    TOKEN_HEADER: 'X-Token'
+}
+
+/**
+ * Use Util object to build URL easier.
+ * @param {string} api 
+ * @param {string} ip 
+ * @param {number} port 
+ * @param {string} app 
+ * @param {string} endpoint 
+ */
+function buildApiUrl(api, ip, port, app, endpoint) {
+    return `${api}://${ip}:${port}/${app}/${endpoint}`;
+}
